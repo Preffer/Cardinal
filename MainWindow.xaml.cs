2118501c8ec62df5813328555d1e400d96cce8f0
Yuzo <hyzgog@gmail.com>
2015-10-12 22:16:31 +0800

    add SizeChanged

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace Cardinal {
@@ -181,6 +173,14 @@ namespace Cardinal {
             emode = EditMode.None;
         }
 
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e) {
+            System.Windows.Media.Matrix scaleMatrix = new System.Windows.Media.Matrix();
+            scaleMatrix.Scale(e.NewSize.Width / e.PreviousSize.Width, e.NewSize.Height / e.PreviousSize.Height);
+
+            InputLine.Points = new PointCollection(InputLine.Points.Select(p => p * scaleMatrix));
+            SmoothLine.Points = new PointCollection(SmoothLine.Points.Select(p => p * scaleMatrix));
+        }
+
         private void SceneChanged(object sender, PropertyChangedEventArgs e) {
             if (e.PropertyName == "InputLine" || e.PropertyName == "Tension" || e.PropertyName == "Grain") {
                 if (InputLine.Points.Count > 0) {
@@ -213,5 +213,6 @@ namespace Cardinal {
 
             return new Point(uhVector * pxVector, uhVector * pyVector);
         }
+
     }
 }
